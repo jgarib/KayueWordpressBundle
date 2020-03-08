ca2a2ad350e740530f2638e2831f4b7088fb5f0c
Jose Garibay <jose@jgarib.com>
2020-03-08 12:17:19 -0400

    add symfony 4 support

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -21,14 +21,19 @@ class Configuration implements ConfigurationInterface
         $rootNode = $treeBuilder->root('kayue_wordpress');
 
         $rootNode->children()
-            ->scalarNode('site_url')->defaultValue(null)->end()
-            ->scalarNode('logged_in_key')->defaultValue(null)->end()
-            ->scalarNode('logged_in_salt')->defaultValue(null)->end()
-            ->scalarNode('cookie_path')->defaultValue('/')->end()
-            ->scalarNode('cookie_domain')->defaultValue(null)->end()
-            ->scalarNode('table_prefix')->defaultValue('wp_')->end()
-            ->scalarNode('connection')->defaultValue('default')->end()
-        ->end();
+                 ->scalarNode('site_url')->defaultValue(null)->end()
+                 ->scalarNode('logged_in_key')->defaultValue(null)->end()
+                 ->scalarNode('logged_in_salt')->defaultValue(null)->end()
+                 ->scalarNode('cookie_path')->defaultValue('/')->end()
+                 ->scalarNode('cookie_domain')->defaultValue(null)->end()
+                 ->scalarNode('table_prefix')->defaultValue('wp_')->end()
+                 ->scalarNode('connection')->defaultValue('default')->end()
+                 ->arrayNode('orm')->addDefaultsIfNotSet()->children()
+                 ->scalarNode('metadata_cache_pool')->defaultValue('cache.system')->end()
+                 ->scalarNode('query_cache_pool')->defaultValue('cache.app')->end()
+                 ->scalarNode('result_cache_pool')->defaultValue('cache.app')->end()
+                 ->end()
+                 ->end();
 
         return $treeBuilder;
     }
